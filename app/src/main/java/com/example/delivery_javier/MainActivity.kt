7486adc0863fbccd69fba83d6910995e0a99cc3f
jA71 <isjavier77@gmail.com>
2022-05-16 17:00:57 -0500

    Se actuliza MainActivity

--- app/src/main/java/com/example/delivery_javier/MainActivity.kt
+++ app/src/main/java/com/example/delivery_javier/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.example.delivery_javier
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
+import com.example.delivery_javier.databinding.ActivityMainBinding
 import android.os.Bundle
+import com.example.delivery_javier.interface_user.Principal
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var views: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        views = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(views.root)
+        initializacionListeners()
+    }
+
+    private fun initializacionListeners() {
+        views.Login.setOnClickListener {
+            val userText = views.User.text.toString()
+            val passwordText = views.Password.text.toString()
+
+            userText?.let {
+                passwordText?.let { itp ->
+                    if (it.equals("jav_ayora") && itp.equals("123456")) {
+                        goToPrincipal(userText)
+                    }
+                }
+            }
+        }
+    }
+
+    private fun goToPrincipal(userText:String) {
+        var intent: Intent = Intent( this, Principal::class.java)
+        intent.putExtra("name_user", userText)
+        startActivity(intent)
     }
 }
--- app/src/main/java/com/example/delivery_javier/interface_user/Principal.kt
+++ app/src/main/java/com/example/delivery_javier/interface_user/Principal.kt
@@ -1,11 +1,24 @@
 package com.example.delivery_javier.interface_user
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.RemoteViews
+import com.example.delivery_javier.databinding.ActivityPrincipalBinding
 
 class Principal : AppCompatActivity() {
+
+    private lateinit var views: ActivityPrincipalBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_principal)
+        views = ActivityPrincipalBinding.inflate(layoutInflater)
+        setContentView(views.root)
+        showName(intent)
+    }
+
+    private fun showName(intent: Intent){
+        val user_name = intent.getStringExtra("name_user")
+        views.textUser.setText(user_name)
     }
 }
