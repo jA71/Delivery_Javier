fe0e284ecec4dd3c4e1643176229e78f9034f50b
jA71 <isjavier77@gmail.com>
2022-05-16 14:16:24 -0500

    Se actualiza proyecto con cambios de interfaces

---
+++ app/src/main/java/com/example/delivery_javier/interface_user/Principal.kt
@@ -0,0 +1,11 @@
+package com.example.delivery_javier.interface_user
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Principal : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_principal)
+    }
+}
